061c05a2cf691aecb6c808f65e85ed918eb9bab6
ostia456 <ostiadedo456@gmail.com>
2026-06-09 10:14:30 +0100

    api base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Configuration de l'API
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1'|| 'https://stata-backend.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
